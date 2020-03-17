406622b8b94dfa603cd8985740be900fa9d97398
SaumilShah66 <shah.saumil66@gmail.com>
2020-03-16 21:23:38 -0400

    Final

---
+++ cylinder_detection/include/cylinder_detection/imgproc.h
@@ -0,0 +1,106 @@
+#ifndef _IMGPROC_H_
+#define _IMGPROC_H_
+
+#include <iostream>
+#include "opencv2/core/core.hpp"
+using namespace std;
+#include <math.h>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+using namespace cv;
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/camera_subscriber.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
+#include <visp/vpConfig.h>
+#include <visp/vpConfig.h>
+#include <visp/vpImage.h>
+#include <visp/vpMeLine.h>
+#include <visp/vpOpenCVGrabber.h>
+#include <visp/vpDisplayOpenCV.h>
+#include <visp/vpDisplayGDI.h>
+#include <visp/vpDisplayX.h>
+#include <visp/vpDot2.h>
+#include <cylinder_msgs/ImageFeatures.h>
+#include <iostream>
+#define pi 3.141592653589
+
+
+class cylinder_detection : public nodelet::Nodelet
+{
+ public:
+	  int image_threshold;
+	  double opt_sizePrecision;
+	  double opt_grayLevelPrecision;
+	  double opt_ellipsoidShapePrecision;
+	  int height_min;
+	  int height_max;
+	  int width_min;
+	  int width_max;
+	  int GrayLevelMin;
+	  int GrayLevelMax;
+	  int Surface;
+	  double fx;
+	  double fy;
+	  double cx;
+	  double cy;
+	  double d0;
+	  double d1;
+	  double d2;
+	  double d3;
+	  int lowThreshold;
+	  int thresh_threshold;
+	  int maxThreshold;
+	  int maxCannyThreshold;
+	  int aperture_size;
+	  double thetaRes;
+	  int HoughThresh;
+	  int minLineLength;
+	  int maxLineGap;
+	  int kernelSize;
+	  int rhoRes;
+	  int sigmaX;
+	   
+
+	  image_transport::Publisher image_thresholded_pub_;
+	    ros::Publisher cylinder_pos_pub_;
+	    cylinder_msgs::ImageFeatures detected_features;
+
+	void onInit(void);
+	int nbLines;
+        vpDot2 dot_search;
+        vpImagePoint init_point_blob;
+	std::vector<vpMeLine*> line_buffer;
+	std::vector<boost::thread*> line_tracker_buff_thread;
+	void imgproc_visp(const cv::Mat &src,  const ros::Time& frame_time);
+        void imgproc_opencv(const cv::Mat &img);
+	void init_detection_hough(const Mat &src, Vec4i& P1, Vec4i& P2, int& size);
+	double P[12];
+	int method;
+	int size;
+	vpMe me;
+
+ private:
+  void camera_callback(const sensor_msgs::ImageConstPtr &img);
+  image_transport::Subscriber sub_camera_;
+};
+
+typedef struct _Patch
+{
+  double x1;
+  double y1;
+  double x2;
+  double y2;
+  double orientation;
+  ros::Time time_computation;
+}Patch;
+
+
+#endif
